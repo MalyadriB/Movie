6a6054eea84aef82e532286215a1a1a39cd3040c
Malyadri <bondikalla@gmail.com>
2023-08-28 20:11:06 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data"; 
+
+function MovieCard({ name, duration, date, rating, img }) {
+  return (
+    <div className="movie-card">
+      <img src={img} alt={name} />
+      <h2>{name}</h2>
+      <p>Duration: {duration}</p>
+      <p>Release Date: {date}</p>
+      <p>Rating: {rating}</p>
+    </div>
+  );
+}
 
 function App() {
+  const [filteredMovies, setFilteredMovies] = useState(data);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const handleDateFilter = (event) => {
+    event.preventDefault();
+
+    const filteredData = data.filter((movie) => {
+      const movieDateParts = movie.date.split("/");
+      const movieYear = parseInt(movieDateParts[2], 10);
+      const movieMonth = parseInt(movieDateParts[1], 10);
+      const movieDay = parseInt(movieDateParts[0], 10);
+
+      const startParts = startDate.split("-");
+      const startYear = parseInt(startParts[0], 10);
+      const startMonth = parseInt(startParts[1], 10);
+      const startDay = parseInt(startParts[2], 10);
+
+      const endParts = endDate.split("-");
+      const endYear = parseInt(endParts[0], 10);
+      const endMonth = parseInt(endParts[1], 10);
+      const endDay = parseInt(endParts[2], 10);
+
+      const movieDate = new Date(movieYear, movieMonth - 1, movieDay);
+      const startDateObj = new Date(startYear, startMonth - 1, startDay);
+      const endDateObj = new Date(endYear, endMonth - 1, endDay);
+
+      return movieDate >= startDateObj && movieDate <= endDateObj;
+    });
+
+    setFilteredMovies(filteredData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleDateFilter}>
+        <input
+          type="date"
+          placeholder="Start Date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+        <input
+          type="date"
+          placeholder="End Date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="movie-list">
+        {filteredMovies.map((movie, index) => (
+          <MovieCard key={index} {...movie} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    name: "rrr",
+    duration: "2 hr 30 min",
+    date: "02/02/2023",
+    rating: "4.3",
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/RRR_Poster.jpg/220px-RRR_Poster.jpg",
+  },
+  {
+    name: "baby",
+    duration: "2 hr 30 min",
+    date: "02/05/2023",
+    rating: "4.3",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/46/Baby_theatrical_poster.jpg",
+  },
+  {
+    name: "rangastalam",
+    duration: "2 hr 30 min",
+    date: "02/07/2023",
+    rating: "4.3",
+    img: "https://cdn.123telugu.com/content/wp-content/uploads/2018/03/Rangastalam-review-m.jpg",
+  },
+  {
+    name: "temper",
+    duration: "2 hr 30 min",
+    date: "04/09/2023",
+    rating: "4.3",
+    img: "https://images.jdmagicbox.com/comp/jd_social/news/2018aug06/image-197597-l86guhdvk9.jpg",
+  },
+];
+export default data;
